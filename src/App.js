6539453efbd79f20765f5e096e8b56e3ff930456
Marco Ziliani <marco.ziliani@musement.com>
2021-02-26 20:21:25 +0100

    feat: add app data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css";
 
+const fakeProducts = require("./mocks/data/products.json");
+
 const data = {
+  title: "Edgemony Shop",
+  description: "A fake e-commerce with a lot of potential",
   logo:
     "https://edgemony.com/wp-content/uploads/2020/03/cropped-Logo-edgemony_TeBIANCO-04.png",
+  cover:
+    "https://images.pexels.com/photos/4123897/pexels-photo-4123897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+  products: fakeProducts,
 };
 
 function App() {
